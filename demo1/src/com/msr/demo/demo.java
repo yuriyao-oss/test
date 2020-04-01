a94b386601099298f1a938a3d13a785a19865416
zhangyao <zhangyao@163.com>
2020-04-01 17:49:37 +0900

    上传DEMO工程

---
+++ demo1/src/com/msr/demo/demo.java
@@ -0,0 +1,11 @@
+package com.msr.demo;
+
+public class demo {
+    public static void main(String[] args) {
+
+        System.out.println(111);
+        System.out.println(222);
+        int key = 1;
+
+    }
+}
