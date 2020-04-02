23e27b1c14f6d5c6517465836466872a9cbae9a3
zhangyao <zhangyao@163.com>
2020-04-02 14:15:57 +0900

    commit

--- test/src/com/msr/lesson/test03.java
+++ test/src/com/msr/lesson/test03.java
@@ -4,5 +4,6 @@ public class test03 {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(3456);
+        System.out.println(789654);
     }
 }
